634663f66d35c22fbbb697facd6051d6145c478c
Jerry Jacobs <jerry.jacobs@xor-gate.org>
2018-08-01 18:01:24 +0200

    golang/grpc-petstore: Initial working POST

--- golang/grpc-petstore/cmd/grpc-petstore-gateway/main.go
+++ golang/grpc-petstore/cmd/grpc-petstore-gateway/main.go
@@ -3,6 +3,7 @@ package main
 import (
   "flag"
   "log"
+  "strings"
   "net/http"
 
   "golang.org/x/net/context"
@@ -27,8 +28,37 @@ func run() error {
     return err
   }
 
+   s := &http.Server{
+		Addr:    ":8080",
+		Handler: allowCORS(mux),
+  }
+
   log.Println("Listening grpc-petstore-gateway on :8080 proxy to", *petstoreEndpoint)
-  return http.ListenAndServe(":8080", mux)
+  return s.ListenAndServe()
+}
+
+// allowCORS allows Cross Origin Resoruce Sharing from any origin.
+// Don't do this without consideration in production systems.
+func allowCORS(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
+				preflightHandler(w, r)
+				return
+			}
+		}
+		h.ServeHTTP(w, r)
+	})
+}
+
+func preflightHandler(w http.ResponseWriter, r *http.Request) {
+	headers := []string{"Content-Type", "Accept"}
+	w.Header().Set("Access-Control-Allow-Headers", strings.Join(headers, ","))
+	methods := []string{"GET", "HEAD", "POST", "PUT", "DELETE"}
+	w.Header().Set("Access-Control-Allow-Methods", strings.Join(methods, ","))
+	log.Printf("preflight request for %s", r.URL.Path)
+	return
 }
 
 func main() {
